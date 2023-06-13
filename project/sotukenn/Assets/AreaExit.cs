c0c6a83e9e6f683df8ccda7d258ae52af91bfbc8
吉澤 公晴 <x22n087@chiba-fjb.ac.jp>
2023-06-13 12:18:29 +0900

    deguti

--- project/sotukenn/Assets/AreaExit.cs
+++ project/sotukenn/Assets/AreaExit.cs
@@ -7,33 +7,39 @@ public class AreaExit : MonoBehaviour
     public string areaToLoad;
     public string areaTransitionName;
     public AreaEntrance theEntrance;
+
     public float waitToLoad = 1f;
     private bool shouldLoadAfterFade;
+
     // Start is called before the first frame update
     void Start()
     {
         theEntrance.transitionName = areaTransitionName;
     }
+
     // Update is called once per frame
     void Update()
     {
-        if (shouldLoadAfterFade)
+        if(shouldLoadAfterFade)
         {
             waitToLoad -= Time.deltaTime;
-            if (waitToLoad <= 0)
+            if(waitToLoad <= 0)
             {
                 shouldLoadAfterFade = false;
                 SceneManager.LoadScene(areaToLoad);
             }
         }
     }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Player")
+        if(other.tag == "Player")
         {
             // SceneManager.LoadScene(areaToLoad);
             shouldLoadAfterFade = true;
+
             GameManager.instance.fadingBetweenAreas = true;
+            
             UIFade.instance.FadeToBlack();
             PlayerController.instance.areaTransitionName = areaTransitionName;
         }
