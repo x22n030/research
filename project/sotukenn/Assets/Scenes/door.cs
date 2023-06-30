6ec0f94977ddb49a48ad8a1670aeff37ecce2824
x22n030 <x22n030@mitsuhashi.ac.jp>
2023-06-30 12:37:10 +0900

    int

---
+++ project/sotukenn/Assets/Scenes/door.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class door : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("map");
+        }
+    }
+}
