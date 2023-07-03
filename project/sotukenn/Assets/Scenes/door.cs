5e8554c5c632245340683a5d707e7e96240e1ac3
x22n030 <x22n030@mitsuhashi.ac.jp>
2023-07-03 15:29:37 +0900

    int

--- project/sotukenn/Assets/Scenes/door.cs
+++ project/sotukenn/Assets/Scenes/door.cs
@@ -3,7 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class door : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D other)
+    void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
