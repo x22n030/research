e75598c8c0437259d15fcc4e946c2abcd19eee63
武智 真輝斗 <x22n049@chiba-fjb.ac.jp>
2023-09-26 10:54:58 +0900

    0926

---
+++ project/sotukenn/Assets/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //各パネルを取得
+    public GameObject lightStandPanel;
+    public GameObject drawerPanel;
+    public GameObject pcPanel;
+
+    //ボタンを押したら該当するパネルを表示
+    public void OnClickLightStandTrigger()
+    {
+        lightStandPanel.SetActive(true);
+    }
+
+    public void OnClickDrawerTrigger()
+    {
+        drawerPanel.SetActive(true);
+    }
+
+    public void OnClickPCTrigger()
+    {
+        pcPanel.SetActive(true);
+    }
+}
