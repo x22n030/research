826952e40af2d5b240bd55f5ea0eb252e46edf94
x22n001 <133626268+x22n001@users.noreply.github.com>
2023-10-05 13:16:04 +0900

    1005

---
+++ project/sotukenn/Assets/Mono School/Resources/Map/GameManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //各パネルを取得
+    public GameObject lightStandPanel;
+    public GameObject drawerPanel;
+    public GameObject pcPanel;
+
+    //ボタンを押したら該当するパネルを表示
+    public void OnClickLightStandTrigger()
+    {
+        lightStandPanel.SetActive(true);
+    }
+
+    public void OnClickDrawerTrigger()
+    {
+        drawerPanel.SetActive(true);
+    }
+
+    public void OnClickPCTrigger()
+    {
+        pcPanel.SetActive(true);
+    }
+
+    //ボタンを押したらパネルをすべて非表示にする
+    public void OnClickBackTrigger()
+    {
+        lightStandPanel.SetActive(false);
+        drawerPanel.SetActive(false);
+        pcPanel.SetActive(false);
+    }
+
+}
