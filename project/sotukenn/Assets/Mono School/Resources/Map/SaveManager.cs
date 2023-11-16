3eb1124fccc6c93616f2888a878c9591e8a69e58
x22n001 <133626268+x22n001@users.noreply.github.com>
2023-11-16 12:41:00 +0900

    1116

---
+++ project/sotukenn/Assets/Mono School/Resources/Map/SaveManager.cs
@@ -0,0 +1,18 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//ゲームデータをセーブロードするもの
+public class SaveManager : MonoBehaviour
+{
+    //セーブの実装
+    //セーブする形式を決める：ture/falseの配列（アイテムを手に入れたかどうか）
+    //セーブクラスを作成：このセーブクラスを端末に保存したりする
+    //セーブ関数を作る
+    //ロード関数を作る
+}
+
+/*class SaveData
+{
+    bool[] getItem = new bool[];
+}
