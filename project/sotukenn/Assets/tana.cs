9447e28149d08971cb925fd466dea83021c6cdab
x22n049 <133626482+x22n049@users.noreply.github.com>
2023-11-16 12:33:53 +0900

    1116

---
+++ project/sotukenn/Assets/tana.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tana : MonoBehaviour
+{
+    //*棚をがクリックしたときにplayerがkey(鍵)を持っていれば消える
+    //　・タイミング :棚をクリックしたとき
+    //　・処理 :消える
+    //　・条件 :playerがkeyを持っている
+
+    //　・タイミング :棚をクリックしたとき : ボタンのOnClickで実装
+    public void OnThis()
+    {
+        //　・処理 :消える :SetActiveで実装
+        bool hasItem = true; 
+        if (hasItem == true)
+        {
+            //　・条件 :playerがkeyを持っている : if文で実装
+            gameObject.SetActive(fales);
+        }
+
+    }
+
+
+
+}
